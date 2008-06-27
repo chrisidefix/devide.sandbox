565d377f72a8aeeefa8fa0e7fe49168a2c484013
cpbotha <cpbotha@localhost>
2008-06-27 22:51:20 +0000

    Problem found, thanks to Mathieu.

--- test_tosh_gdcm.py
+++ test_tosh_gdcm.py
@@ -1,13 +1,19 @@
 # script to demonstrate difference in behaviour of GDCM with toshiba
-# dataset on 32 bit Linux/Windows and on 64 bit Linux
+# dataset on Windows (MIP is okay) and on Linux (MIP is empty!)
 
+# bang, this was due to a dl / DLFCN thing on AMD64.  You can't import
+# vtk before vtkgdcm, then it screws up.  Apply this patch to fix:
+#http://public.kitware.com/cgi-bin/viewcvs.cgi/Wrapping/Python/vtk/__init__.py?r1=1.13&r2=1.14&pathrev=MAIN
+
+import vtkgdcm
 import gdcm
 import vtk
-import vtkgdcm
 
 r = vtkgdcm.vtkGDCMImageReader()
 r.SetFileName('tosh_data')
 r.Update()
 
 print r.GetMedicalImageProperties()
+print 'DIMENSIONS:', r.GetOutput().GetDimensions()
+print r.GetNumberOfScalarComponents()
 
